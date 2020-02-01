490ed3e3412953dad247e23ba1f6be443b16354f
jcarter15 <carter.jd@hotmail.co.uk>
2020-02-01 21:24:14 +0000

    Fixed array display issue
    spyder 4.01 installed
    Column transformer replaced categorical features

--- ann.py
+++ ann.py
@@ -13,16 +13,26 @@ X = dataset.iloc[:, 3:13].values
 y = dataset.iloc[:, 13].values
 
 # Encoding categorical data
-"categories='auto'"
+
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+from sklearn.compose import ColumnTransformer, make_column_transformer
+
+
 labelencoder_X_1 = LabelEncoder()
 X[:, 1] = labelencoder_X_1.fit_transform(X[:, 1])
 
 labelencoder_X_2 = LabelEncoder()
 X[:, 2] = labelencoder_X_2.fit_transform(X[:, 2])
 
-onehotencoder = OneHotEncoder(categorical_features = [1])
-X = onehotencoder.fit_transform(X).toarray()
+
+preprocess = make_column_transformer(
+
+    (OneHotEncoder(categories='auto'), [1]),
+
+    remainder="passthrough")
+
+X = np.array(preprocess.fit_transform(X), dtype=np.int)
+
 X = X[:, 1:]
 
 
